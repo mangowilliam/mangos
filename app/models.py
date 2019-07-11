fc04f9cec5a8e4cd236c18a6d562335ae14e88e6
johnwanjema <jonwanjema@gmail.com>
2019-07-11 11:37:41 +0300

    added question models

--- app/models.py
+++ app/models.py
@@ -1,20 +1,22 @@
 from . import db
-from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
 from . import login_manager
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(UserMixin,db.Model):
-    
+
+class User(UserMixin, db.Model):
+
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255),index = True)
-    email = db.Column(db.String(255),unique = True,index = True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), index=True)
+    email = db.Column(db.String(255), unique=True, index=True)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     password_hash = db.Column(db.String(255))
@@ -27,10 +29,8 @@ class User(UserMixin,db.Model):
     def password(self, password):
         self.password_hash = generate_password_hash(password)
 
-
-    def verify_password(self,password):
-        return check_password_hash(self.password_hash,password)
-
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def save_user(self):
         db.session.add(self)
@@ -38,6 +38,7 @@ class User(UserMixin,db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+
 
 class Code(db.Model):
     __tablename__ = 'codes'
@@ -51,21 +52,21 @@ class Code(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     comment_id = db.relationship("Comment", backref="codes", lazy="dynamic")
 
-    def save_blog(self):
+    def save_code(self):
         db.session.add(self)
         db.session.commit()
 
     @classmethod
-    def get_blog(cls, id):
-        blog = Code.query.filter_by(id=id).first()
-        return blog
+    def get_code(cls, id):
+        code = Code.query.filter_by(id=id).first()
+        return code
 
     @classmethod
     def get_codes(cls, id):
         codes = Code.query.filter_by(id=id).all()
         return codes
 
-    def delete_blog(self):
+    def delete_code(self):
         db.session.delete(self)
         db.session.commit()
 
@@ -87,6 +88,34 @@ class Comment(db.Model):
 
     @classmethod
     def get_comments(self, id):
-        comments = Comment.query.filter_by(code_id =id).all()
+        comments = Comment.query.filter_by(code_id=id).all()
         return comments
 
+
+class Quiz(db.Model):
+    __tablename__ = 'quizes'
+    id = db.Column(db.Integer, primary_key=True)
+    quiz_title = db.Column(db.String)
+    question = db.Column(db.String)
+    category = db.Column(db.Integer)
+    posted = db.Column(db.Time, default=datetime.utcnow())
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    comment_id = db.relationship("Comment", backref="codes", lazy="dynamic")
+
+    def save_quiz(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_quiz(cls, id):
+        quiz = Quiz.query.filter_by(id=id).first()
+        return quiz
+
+    @classmethod
+    def get_quizes(cls, id):
+        quizes = Quiz.query.filter_by(id=id).all()
+        return quizes
+
+    def delete_quiz(self):
+        db.session.delete(self)
+        db.session.commit()
