a324cd2a7a70ec1090f4091c29c06d1088c4cc25
Soniawangui <koisonia99@gmail.com>
2019-07-11 10:27:10 +0300

    Add Python cheatsheet

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
@@ -13,9 +12,9 @@ login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
-def create_app(config_state):
+def create_app(config_name):
     app = Flask(__name__)
-    app.config.from_object(config_options[config_state])
+    app.config.from_object(config_options[config_name])
 
 
     bootstrap.init_app(app)
--- manage.py
+++ manage.py
@@ -1,5 +1,4 @@
 import unittest
-from flask_script import Manager,Server
 from app import create_app,db
 from  flask_migrate import Migrate, MigrateCommand
 
